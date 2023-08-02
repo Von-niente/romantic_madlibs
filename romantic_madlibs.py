8c3f7e1836b6fca974016c3d050462986c638e1c
von-niente <laura.natha24@gmail.com>
2023-08-02 13:34:39 -0500

    se agrega el proyecto

---
+++ romantic_madlibs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+sust1 = input("Sustantivo#1: ")
+sust2 = input("Sustantivo#2: ")
+verb1 = input("Verbo#1: ")
+verb2 = input("Verbo#2: ")
+adj1 = input("Adjetivo de modo (termina en mente): ")
+adj2 = input("Adjetivo #2: ")
+verb3 = input ("Verbo 2da persona: ")
+
+madlib= f"Hola… primero que nada, hoy quiero {verb1} en agradecimiento y te envío un {sust1} enorme, para así comenzar a decirte tantas cosas que quisiera expresar, pero que tal vez no me he atrevido a hacerlo, y a pesar de tan poco tiempo, ya te has metido en mi {sust2} de una manera inexplicable y no hago otra cosa que {verb2} en ti, el haberte conocido es algo que me cambio {adj1}, para bien cabe destacar. Eres la persona más {adj2} que he conocido, me encanta todo, absolutamente todo de ti, Te {verb3}."
+
+print(madlib) 
